262a259c1528a061959dd7b9c6deecdf5d3d28fa
Kenneth-Tuan <pokemenggou0.0@gmail.com>
2023-12-15 10:10:50 +0800

    wip: add more hymns dummy data; update hymn sheet selector

--- src/components/HymnsTable.vue
+++ src/components/HymnsTable.vue
@@ -1,5 +1,26 @@
 <script setup>
+import { ref } from "vue";
 import { keys } from "@/constants/misc";
+import {
+  dummyHymns,
+  dummyHymnDescription,
+  dummyHymnSheet,
+} from "@/mocks/domains/hymn/data";
+
+// const stringOptions = ["Google", "Facebook", "Twitter", "Apple", "Oracle"];
+
+const stringOptions = [
+  "Google",
+  "Facebook",
+  "Twitter",
+  "Apple",
+  "Oracle",
+].reduce((acc, opt) => {
+  for (let i = 1; i <= 5; i++) {
+    acc.push(opt + " " + i);
+  }
+  return acc;
+}, []);
 
 const props = defineProps({
   hymns: {
@@ -7,6 +28,34 @@ const props = defineProps({
     required: true,
   },
 });
+
+const options = ref(stringOptions);
+const model = ref(null);
+
+function filterFn(val, update, abort) {
+  if (val.length < 2) {
+    abort();
+    return;
+  }
+  update(() => {
+    const needle = val.toLocaleLowerCase();
+    options.value = stringOptions.filter(
+      (v) => v.toLocaleLowerCase().indexOf(needle) > -1
+    );
+  });
+}
+
+function setModel(val) {
+  model.value = val;
+}
+
+function highlightFiltered(opt) {
+  const label = opt.label.replaceAll(
+    props.modelValue?.toUpperCase(),
+    `<span style='font-weight: 600 !important'>${props.modelValue?.toUpperCase()}</span>`
+  );
+  return label;
+}
 </script>
 
 <template>
@@ -15,8 +64,34 @@ const props = defineProps({
     class="u-flex u-flex-row u-items-center u-justify-start u-flex-nowrap u-gap-x-16px"
   >
     <span>{{ index + 1 }}</span>
-    <q-input v-model="hymn.name" type="text" label="hymn_name" outlined dense />
-    <!-- <q-select v-model="hymn.name" :options="options" label="Standard" filled /> -->
+
+    <q-select
+      :model-value="model"
+      use-input
+      hide-selected
+      fill-input
+      input-debounce="0"
+      :options="options"
+      @filter="filterFn"
+      @input-value="setModel"
+      label="hymn_name"
+      outlined
+      dense
+    >
+      <template v-slot:no-option>
+        <q-item>
+          <q-item-section class="text-grey"> No results </q-item-section>
+        </q-item>
+      </template>
+      <template v-slot:option="scope">
+        <q-item v-bind="scope.itemProps">
+          <q-item-section>
+            <q-item-label v-html="highlightFiltered(scope.opt)" />
+          </q-item-section>
+        </q-item>
+      </template>
+    </q-select>
+
     <q-select
       v-model="hymn.key"
       :options="keys"
@@ -34,12 +109,23 @@ const props = defineProps({
 
     <q-select
       v-model="hymn.sheet"
-      :options="keys"
-      type="text"
+      :options="dummyHymnSheet"
+      map-options
+      options-html
       outlined
       dense
       label="hymn_sheet_version"
     >
+      <template v-slot:option="scope">
+        <q-item v-bind="scope.itemProps">
+          <q-item-section>
+            {{ scope.opt.file_type_dsp }}
+          </q-item-section>
+        </q-item>
+      </template>
+      <template v-slot:selected-item="scope">
+        {{ scope.opt.file_type_dsp }}
+      </template>
     </q-select>
 
     <q-btn flat dense color="primary" :ripple="false" no-caps>
